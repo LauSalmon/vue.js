a61f4ef445f05a3c80886355c59abe08c8f47f43
LauSalmon <salmon.laura@laposte.net>
2023-11-29 16:18:39 +0100

    modif exo-toutCasser/app.js

--- Exo-toutCasser/app.js
+++ Exo-toutCasser/app.js
@@ -24,3 +24,34 @@ Vue.createApp({
         }
     }
 }).mount('#event');
+
+
+// Soluce avec COMPUTED
+// ! Avec les computed properties, déormais VUE va analyser en détail nos fonctions et il ne les execute
+// ! uniquement si il y a un changement sur une ou plusieurs variables utilisée dans la dite fonction
+// const app = Vue.createApp({
+//     data() {
+//         return { 
+//         leNombre:0,nameUser: ''};
+//     },
+//     methods: {
+//         augmenter(num){
+//         this.leNombre += num;
+//     },
+//     reduire(num){
+//         this.leNombre -= num;
+//     },
+//     },
+//     computed:{
+//         afficherNameUser() {
+//         console.log('fonction executée');
+//         if(this.nameUser ===''){
+//             return 'test'
+//         }
+//         else{
+//             return 'autre Test';
+//         }
+//         },
+//     }
+//     });
+//    app.mount('#monApp');
