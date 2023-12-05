f8cb244ce5ac7fbd99af2ccafe52f4964e361e44
LauSalmon <salmon.laura@laposte.net>
2023-12-05 11:59:17 +0100

    ajout dossier tp-listeCourses

---
+++ tp-listeCourses/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data() {
+        return {
+        article: '',
+        mesArticles: [],
+        hidden: false,
+        afficher: true
+        };
+    },
+    methods: {
+        //Méthode pour ajouter des éléments dans la liste
+        ajouterArticle() {
+        this.mesArticles.push(this.article);
+        },
+        //Méthode pour supprimer des éléments de la liste
+        supprimerArticle(unIndex) {
+        this.mesArticles.splice(unIndex, 1);
+        },
+        //Méthode pour cacher la liste de courses + modifier ce qu'il y a écrit sur le bouton
+        cache (){
+            this.hidden = !this.hidden;
+            this.afficher = !this.afficher;
+        },
+    },
+    });
+    app.mount('#monApp');
